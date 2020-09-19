b787223423d5c6abe59fdd67ff24d46747c5e64a
99002455 <71481658+99002455@users.noreply.github.com>
2020-09-19 09:51:03 +0530

    Update calculator_operations.c

--- final-master/src/calculator_operations.c
+++ final-master/src/calculator_operations.c
@@ -1,21 +1,21 @@
 #include <calculator_operations.h>
 
-int add(int operand1, int operand2)
+int add1(int operand1, int operand2)
 {
     return operand1 + operand2;
 }
 
-int subtract(int operand1, int operand2)
+int subtract1(int operand1, int operand2)
 {
     return operand1 - operand2;
 }
 
-int multiply(int operand1, int operand2)
+int multiply1(int operand1, int operand2)
 {
     return operand1 * operand2;
 }
 
-int divide(int operand1, int operand2)
+int divide1(int operand1, int operand2)
 {
     if(0 == operand2)
         return 0;
@@ -23,7 +23,7 @@ int divide(int operand1, int operand2)
         return operand1 / operand2;
 }
 
-int greater(int operand1, int operand2)
+int greater1(int operand1, int operand2)
 {
     if(operand1>operand2)
         return operand1;
@@ -31,7 +31,7 @@ int greater(int operand1, int operand2)
         return operand2;
 }
 
-int smaller(int operand1, int operand2)
+int smaller1(int operand1, int operand2)
 {
     if(operand1<operand2)
         return operand1;
@@ -39,7 +39,7 @@ int smaller(int operand1, int operand2)
         return operand2;
 }
 
-int primenum(int operand1)
+int primenum1(int operand1)
 {
     int i,m=0,flag=0;    
       
@@ -57,7 +57,7 @@ int primenum(int operand1)
     return 1;  
 }
     
-int primenum1(int operand2)
+int primenum11(int operand2)
 {
     int i,m=0,flag=0;    
       
@@ -75,7 +75,7 @@ int primenum1(int operand2)
     return 1;  
 }
 
-int evenodd(int operand1)
+int evenodd1(int operand1)
 {
     if(operand1%2==0)
         return 1;
@@ -83,7 +83,7 @@ int evenodd(int operand1)
         return 0; 
 }
 
-int evenodd1(int operand2)
+int evenodd11(int operand2)
 {
     if(operand2%2==0)
         return 1;
@@ -91,7 +91,7 @@ int evenodd1(int operand2)
         return 0; 
 }
 
-int posneg(int operand1)
+int posneg1(int operand1)
 {
     if(operand1>0)
         return 1;
@@ -101,7 +101,7 @@ int posneg(int operand1)
         return -1;
 }
 
-int posneg1(int operand2)
+int posneg11(int operand2)
 {
     if(operand2>0)
         return 1;
@@ -111,7 +111,7 @@ int posneg1(int operand2)
         return -1;
 }
 
-int factorial(int operand1)
+int factorial1(int operand1)
 {
    if(operand1 < 0)
     return -1;
@@ -120,7 +120,7 @@ int factorial(int operand1)
 return operand1 * factorial(operand1-1);
 }
 
-int factorial1(int operand2)
+int factorial11(int operand2)
 {
    if(operand2 < 0)
     return -1;
@@ -129,34 +129,34 @@ int factorial1(int operand2)
 return operand2 * factorial(operand2-1);
 }
 
-int squarearea(int operand1)
+int squarearea1(int operand1)
 {
     return operand1*operand1;
 }
 
-int squareperi(int operand1)
+int squareperi1(int operand1)
 {
     return 4*operand1;
 }
 
-int square(int operand1)
+int square1(int operand1)
 {
     return operand1*operand1;
 }
-int square1(int operand2)
+int square11(int operand2)
 {
     return operand2*operand2;
 }
-int cube(int operand1)
+int cube1(int operand1)
 {
     return operand1*operand1*operand1;
 }
-int cube1(int operand2)
+int cube11(int operand2)
 {
     return operand2*operand2*operand2;
 }
 
-int reverse(int operand1)
+int reverse1(int operand1)
 {
     int rev=0,remainder;
     while(operand1!=0){
@@ -166,7 +166,7 @@ int reverse(int operand1)
     }
     return rev;
 }
-int reverse1(int operand2)
+int reverse11(int operand2)
 {
     int rev=0,remainder;
     while(operand2!=0){
@@ -176,7 +176,7 @@ int reverse1(int operand2)
     }
     return rev;
 }
-int palindrome(int operand1)
+int palindrome1(int operand1)
 {
     int rev=0,remainder,original;
     original = operand1;
@@ -190,7 +190,7 @@ int palindrome(int operand1)
     else
         return 0;
 }
-int palindrome1(int operand2)
+int palindrome11(int operand2)
 {
     int rev=0,remainder,original;
     original = operand2;
@@ -204,7 +204,7 @@ int palindrome1(int operand2)
     else
         return 0;
 }
-int gcd(int operand1, int operand2)
+int gcd1(int operand1, int operand2)
 {
     int count,gcd;
     for(count=1;count<=operand1 && count<=operand2;++count)
@@ -214,7 +214,7 @@ int gcd(int operand1, int operand2)
     }
     return gcd;
 }
-int lcm(int operand1, int operand2)
+int lcm1(int operand1, int operand2)
 {
     int maximum;
     maximum=(operand1>operand2)?operand1:operand2;
@@ -228,23 +228,23 @@ int lcm(int operand1, int operand2)
         ++maximum;
     }
 }
-int modulus(int operand1,int operand2)
+int modulus1(int operand1,int operand2)
 {
     int remainder;
     remainder= operand1%operand2;
     return remainder;
 }
-int kilometertometer(int operand1)
+int kilometertometer1(int operand1)
 {
     int meter=operand1*1000;
     return meter;
 }
-int kilotocentimeter(int operand1)
+int kilotocentimeter1(int operand1)
 {
     int centimeter = operand1*100000;
     return centimeter;
 }
-int kilotomillimeter(int operand1)
+int kilotomillimeter1(int operand1)
 {
     int millimeter = operand1*1000000;
     return millimeter;
